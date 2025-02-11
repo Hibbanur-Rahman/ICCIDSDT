a90aa0a0465150e1c302e45bb717f86ffb4b03e5
Hibbanur-Rahman <hibbanrahmanhyt@gmail.com>
2025-02-11 15:56:40 +0530

    change the name of footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,8 +4,8 @@ const Footer = () => {
   return (
     <footer className="bg-blue-950 text-white py-8">
       <div className="container mx-auto px-4 text-center">
-        <p>© 2024 ICLT 2025. All rights reserved.</p>
-        <p className="mt-2">Organized by MANUU Law School</p>
+        <p>© 2024 ICCIDSDT 2025. All rights reserved.</p>
+        <p className="mt-2">Organized by Dept. Computer Science & Information Technology</p>
         <p className="mt-2">Maulana Azad National Urdu University, Hyderabad</p>
       </div>
     </footer>
